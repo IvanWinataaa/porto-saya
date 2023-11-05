10e28a54cdae05f6ea3b2f683e945c5fbb36e907
IvanWinataa <146960260+IvanWinataa@users.noreply.github.com>
2023-11-05 16:42:03 +0800

    menambahkan tipe kepribadian hello.c

--- hello.c
+++ hello.c
@@ -7,6 +7,7 @@ printf("tinggi badan saya 178cm");
 printf("Hobi saya nonton film");
 printf("Cita-cita saya jadi orang sukses");
 printf("Makanan kesukaan saya adalah nasi goreng");
+printf("Saya tipe kepribadian introvert");
   return 0;
   
 }
